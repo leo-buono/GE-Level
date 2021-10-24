6c940065f05d583b0301cb72858b81ff6c190ccc
leo-buono <56273373+leo-buono@users.noreply.github.com>
2021-10-24 19:11:40 -0400

    All basic platforms added

---
+++ GE_Lab_2/GE_Project/Assets/Scripts/FactoryDesign/CheckPointFactory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointFactory : FactoryDesign<MonoBehaviour>
+{
+    public CheckPointFactory()
+    {
+        
+    }
+}
