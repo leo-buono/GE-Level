470a752ee576cc4623e76378f3ee0b12b1e5b49f
Jaden <jaden.bhakti@ontariotechu.net>
2021-10-25 19:01:20 -0400

    timer

--- GE_Lab_2/GE_Project/Assets/Scripts/WinScript.cs
+++ GE_Lab_2/GE_Project/Assets/Scripts/WinScript.cs
@@ -6,12 +6,36 @@ using UnityEngine.UI;
 public class WinScript : MonoBehaviour
 {
     public GameObject winText;
- 
+    public Text textTimer;
+    private float startTime;
+    private bool theEnd = false;
+
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (theEnd)
+            return;
+
+
+        float t = Time.time - startTime;
+
+        string min = ((int)t / 60).ToString();
+        string sec = (t % 60).ToString("f2");
+
+        textTimer.text = min + ":" + sec;
+
+
+    }
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.tag == "Player")
         {
             winText.GetComponent<TextMesh>().text = "You win!!!";
+            theEnd = true;
         }
     }
 }
