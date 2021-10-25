60239dffee663f3f413a404c75a63f85c75c3395
Jaden <jaden.bhakti@ontariotechu.net>
2021-10-24 20:15:14 -0400

    dropdown working

---
+++ GE_Lab_2/GE_Project/Assets/Scripts/UI/dropdown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dropdown : MonoBehaviour
+{
+    public Text TextBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var dropdown = transform.GetComponent<Dropdown>();
+
+        dropdown.options.Clear();
+
+        List<string> platforms = new List<string>();
+        platforms.Add("Checkpoint");
+        platforms.Add("Rotating");
+        platforms.Add("Disappearing");
+        platforms.Add("End");
+        platforms.Add("Generic");
+
+        //for putting platform types in the dropdown menu
+        foreach(var platform in platforms)
+        {
+            dropdown.options.Add(new Dropdown.OptionData() { text = platform });
+        }
+
+        DropdownItemPressed(dropdown);
+
+        dropdown.onValueChanged.AddListener(delegate { DropdownItemPressed(dropdown); });
+        
+    }
+
+    void DropdownItemPressed (Dropdown dropdown)
+    {
+        int index = dropdown.value;
+
+        TextBox.text = dropdown.options[index].text;
+    }
+
+}
